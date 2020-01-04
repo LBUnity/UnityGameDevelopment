0e2654f6e4fc9623ffc4bb4e919a42ae788d1136
LBayard007 <lukasbayard@hotmail.com>
2020-01-04 18:38:34 -0500

    Section 1 tuitorial

---
+++ MyFirstProject/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform m_pumpkinPos;
+    private float m_zDistance = 7f;
+    private float m_yDistance = 4f;
+
+    private void Awake()
+    {
+        m_pumpkinPos = GameObject.Find("Pumpkin").transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        temp.y = m_pumpkinPos.position.y + m_yDistance;
+        temp.z = m_pumpkinPos.position.z - m_zDistance;
+        transform.position = temp;
+    }
+}
---
+++ MyFirstProject/Assets/Scripts/Pumpkin.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkin : MonoBehaviour
+{
+    public float MoveForce = 10f;
+    private Rigidbody m_myBody;
+
+    //called before start()
+    private void Awake()
+    {
+        m_myBody = GetComponent<Rigidbody>();    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Updated every 4-5th frame
+    void FixedUpdate()
+    {
+        MovePumpkin();
+    }
+
+    void MovePumpkin()
+    {
+        float h = Input.GetAxis("Horizontal"); //changes when you press the left, right arrow keys
+        float v = Input.GetAxis("Vertical"); //changes when you press the up, down arrow keys
+
+        m_myBody.AddForce(new Vector3(h * MoveForce, 0f, v * MoveForce));
+    }
+
+    private void OnTriggerEnter(Collider _target)
+    {
+        if (_target.tag == "Golem" || _target.tag == "Gates")
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
